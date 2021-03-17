7429141e28daa968f02b8351c36e510dc5bfb221
Cyan0n <cyan0n.dev@gmail.com>
2021-03-18 00:07:30 +0100

    Added discord hook

--- components/rollButton.tsx
+++ components/rollButton.tsx
@@ -1,5 +1,20 @@
 import { ReactElement } from 'react'
+import axios from 'axios'
+
+const url = 'https://discord.com/api/webhooks/820970895547957308/m9oTYkTFeTnkxxkh0Idow3CkDYM90Vem5qzAF4YR_wE1sn413-cY1lciOA6sAyT-y4du'
+
+function send() {
+  console.log("SEND")
+  axios.post(url, {
+    content: "Hello world!",
+    tts: true
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
 
 export default function RollButton(): ReactElement {
-  return <button>Press me</button>
+  return <button onClick={() => send()}>Press me</button>
 }
---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from 'react';
+import RollButton from '../components/rollButton'
+
+export default function TestPage(): ReactElement {
+  return (
+    <>
+      <RollButton/>
+    </>
+  )
+}
