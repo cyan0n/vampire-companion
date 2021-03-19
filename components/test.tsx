bc3a57b34e1c071ff1be3744f31c74420cae7f96
Thomas Cipriani <thomas.cipriani@esimple.it>
2021-03-19 11:35:11 +0100

    Added test for direct webhook

--- components/test.tsx
+++ components/test.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+const DISCORD_WEBHOOK = process.env.NEXT_PUBLIC_DISCORD_WEBHOOK
+
 export default function Test(): React.ReactElement {
+
 	const send = async () => {
-		await fetch('/api/send', {
-			body: JSON.stringify({ msg: "hello thomas!" }),
+		console.log(DISCORD_WEBHOOK)
+		await fetch(DISCORD_WEBHOOK, {
+			body: JSON.stringify({content: "hello test"}),
 			method: "POST",
+			headers: {
+				'Content-Type': 'application/json',
+			},
 		})
 	}
 	return <button onClick={send}>Press Test</button>
